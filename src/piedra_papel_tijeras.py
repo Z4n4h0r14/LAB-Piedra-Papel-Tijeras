e577b459203645a8c323d286cd97a489f668361f
Z4n4h0r14 <8joseaml@gmail.com>
2024-10-01 19:14:26 +0200

    Queda el error por resolver

--- src/piedra_papel_tijeras.py
+++ src/piedra_papel_tijeras.py
@@ -5,12 +5,23 @@ def ordenador_decide_jugada():
     Elige aleatoriamente entre piedra, papel o tijeras y devuelve la elección.     
     '''
     opciones = ["piedra", "papel", "tijeras"]
-    res = random.choice(opciones)
-    return res
+    res = random.choice(opcion)
 
-def usuario_decide_jugada():
+    def usuario_decide_jugada():
     ''' 
     Pide al usuario que elija entre piedra, papel o tijeras y devuelve la elección.     
     '''
     eleccion_usuario = input("Elige piedra, papel o tijeras: ")    
     return eleccion_usuario
+
+def determina_ganador(jugada_usuario, jugada_ordenador):
+    if jugada_usuario == jugada_ordenador:
+        return "Empate"
+    elif jugada_usuario == "piedra" and jugada_ordenador == "tijeras":
+        return "Ganaste"
+    elif jugada_usuario == "tijera" and jugada_ordenador == "papel":
+        return "Ganaste"
+    elif  jugada_usuario == "papel" and jugada_ordenador == "piedra":
+        return "Ganaste"
+    else:
+        return "Perdiste"
