a39af2e62de655d30c605b000029f03678c923c0
Z4n4h0r14 <8joseaml@gmail.com>
2024-10-07 11:22:49 +0200

    Funciona

--- src/piedra_papel_tijeras.py
+++ src/piedra_papel_tijeras.py
@@ -6,11 +6,16 @@ def ordenador_decide_jugada():
     '''
     opciones = ["piedra", "papel", "tijeras"]
     res = random.choice(opciones)
+    return res
 
-def usuario_decide_jugada(eleccion_usuario):
-
-     eleccion_usuario = input("Elige piedra, papel o tijeras: ")    
-     return eleccion_usuario
+def usuario_decide_jugada():
+    ''' 
+    Pide al usuario que elija entre piedra, papel o tijeras y devuelve la elección.     
+    '''
+    eleccion_usuario = input("Elige piedra, papel o tijeras: ")
+    while eleccion_usuario not in ["piedra", "papel", "tijeras"]:
+        eleccion_usuario = input("Opción no válida, por favor elige piedra, papel o tijeras: ")
+    return eleccion_usuario
 
 def determina_ganador(jugada_usuario, jugada_ordenador):
     if jugada_usuario == jugada_ordenador:
@@ -23,3 +28,26 @@ def determina_ganador(jugada_usuario, jugada_ordenador):
         return "Ganaste"
     else:
         return "Perdiste"
+    
+def jugar():
+    print("Bienvenido Jugador")
+    ordenador_decide_jugada()
+    jugador = usuario_decide_jugada()
+    ordenador = ordenador_decide_jugada()
+    print("La eleccion del ordenador ha sido:",ordenador)
+    determina_ganador(jugador , ordenador)
+    Ganador = determina_ganador(jugador,ordenador)
+    print(Ganador)
+    if Ganador == "Ganaste":
+        print("Has ganado")
+    else:
+        print("vuelve a intentarlo")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    jugar()
